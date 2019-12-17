e956efcd54307b8bb22051e81b78b91c840a7386
jeanpierremartine <Ptizef>
2019-12-17 18:41:19 +0100

    update .gitignore

--- build/CMakeFiles/navdata-i18n.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/navdata-i18n.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/navdata-pot-update.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/navdata-pot-update"
-  "po/navdata_pi.pot.dummy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/navdata-pot-update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
